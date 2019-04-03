0832b20f3301cf3085dee05a28cf2ef0f1328ea4
madonkey <dermot.s.hughes@gmail.com>
2019-04-04 00:33:36 +0100

    Init commit

---
+++ parser.js
@@ -0,0 +1,18 @@
+const hb = require('handlebars');
+const mjml2html = require('mjml');
+const fs = require('fs');
+
+const fileIn = 'template';
+let mjml = (fs.readFileSync(`${fileIn}.mjml`)).toString();
+let data = {
+    message: 'Hello world'
+}
+
+function compileTemplate(template, data) {
+    let t = hb.compile(template);
+    template = t(data);
+    let output = mjml2html(template, { minify: true });
+    fs.writeFile(`${fileIn}_parsed.html`, output.html);
+}
+
+compileTemplate(mjml, data);
